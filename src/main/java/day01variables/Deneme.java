e3f30f178414a19957a5efde817f56ba73bc611d
ozlem asimoglu <ozlemasimoglu@gmail.com>
2022-11-05 18:24:00 +0100

    first commit

---
+++ src/main/java/day01variables/Deneme.java
@@ -0,0 +1,19 @@
+package day01variables;
+
+public class Deneme {
+
+    public static void main(String[] args) {
+        String cumle = "Nerede o 'hello word ' yazdiramayan arkadaslar";
+        //verilen bir cumledeistenen harfin kac defa kullanildigini yazdiran bir method olusturun
+    }
+
+        public static void harfKarakterKacKereKullanilmis (String cumle,char harf){
+        int sayi = 0;
+        String harfstr=""+harf;
+        String karakterler[]=cumle.split("");
+
+
+
+    }
+
+}
